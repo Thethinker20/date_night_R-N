0d7617bd6c9c9fa9366cce2c5ce81dd335faba74
Thethinker20 <43387401+Thethinker20@users.noreply.github.com>
2022-10-23 23:36:01 -0400

    new

---
+++ src/public/media/js/script.js
@@ -0,0 +1,29 @@
+function init() {
+
+    const loader = document.querySelector('.container');
+const main = document.querySelector('.header_p');
+
+    setTimeout(() => {
+      loader.style.opacity = 0;
+      loader.style.display = 'none';
+  
+      main.style.display = 'block';
+      setTimeout(() => main.style.opacity = 1, 100);
+    }, 1000);
+  }
+  
+  init();
+
+
+$(".choco").on("click", function(){
+    const header_p = document.querySelector('.header_p');
+    const main_page = document.querySelector('.main_page');
+
+    setTimeout(() => {
+        header_p.style.opacity = 0;
+        header_p.style.display = 'none';
+    
+        main_page.style.display = 'block';
+        setTimeout(() => main_page.style.opacity = 1, 100);
+      }, 1000);
+});
